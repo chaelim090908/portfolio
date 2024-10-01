55346c450882657e92c35d7e66b7971cfefb71eb
chaelim090908 <cofla090908@gmail.com>
2024-10-01 16:33:42 +0900

    241001 page 수정

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	const swiperV = new Swiper('.swiperV', { /* 팝업을 감싼는 요소의 class명 */
+	const swiperV = new Swiper('.swiperV', {
 		direction: "vertical",
 		slidesPerView: 1,
 		spaceBetween: 0,
@@ -16,21 +16,21 @@ $(document).ready(function(){
 				}else if(this.realIndex == 1){
 					$('.page ul li').removeClass('on')
 					$('.page ul li:nth-child(2)').addClass('on')
-					}else if(this.realIndex == 2){
+				}else if(this.realIndex == 2){
 					$('.page ul li').removeClass('on')
-					if(this.previousIndex == 1){//위에서 아래로 내려왔을때
+					$('.page ul li:nth-child(7)').addClass('on')
+				if(this.previousIndex == 1){//위에서 아래로 내려왔을때
+					$('.page ul li').removeClass('on')
 					$('.page ul li:nth-child(3)').addClass('on')
 				}else{//아래에서 위로 올라왔을때
+					$('.page ul li').removeClass('on')
 					$('.page ul li:nth-last-child(2)').addClass('on')
 					}
-				}else if(this.realIndex == 3){
-					$('.page ul li').removeClass('on')
-					$('.page ul li:last-child').addClass('on')
 				}
 			}
 		},
 	});
-	const swiperH = new Swiper('.swiperH', { /* 팝업을 감싼는 요소의 class명 */
+	const swiperH = new Swiper('.swiperH', {
 		direction: "horizontal",
 		slidesPerView: 1,
 		spaceBetween: 0,
@@ -51,34 +51,47 @@ $(document).ready(function(){
 				}else if(this.realIndex == 2){
 					$('.page ul li').removeClass('on')
 					$('.page ul li:nth-child(5)').addClass('on')
+				}else if(this.realIndex == 3){
+					$('.page ul li').removeClass('on')
+					$('.page ul li:nth-child(6)').addClass('on')
 				}
 			}
 		},
 	});
-    $('.page a[data-href="portfolio"]').on('click', function(){
+	$('.page ul li a.portfolio').ready(
+		$('.page ul li:nth-child(1)').addClass('on')
+	)
+    $('.page ul li a.portfolio').on('click', function(){
 		swiperV.slideTo(0, 500);
+		$('.page ul li:nth-child(1)').addClass('on')
 	});
-	$('.page a[data-href="profile"]').on('click', function(){
+	$('.page ul li a.profile').on('click', function(){
 		swiperV.slideTo(1, 500);
+		$('.page ul li:nth-child(2)').addClass('on')
 	});
-	$('.page a[data-href="first"]').on('click', function(){
+	$('.page ul li a.first').on('click', function(){
 		swiperV.slideTo(2, 500);
 		swiperH.slideTo(0, 500);
+		$('.page ul li:nth-child(3)').addClass('on')
 	});
-	$('.page a[data-href="second"]').on('click', function(){
+	$('.page ul li a.second').on('click', function(){
 		swiperV.slideTo(2, 500);
 		swiperH.slideTo(1, 500);
+		$('.page ul li:nth-child(4)').addClass('on')
 	});
-	$('.page a[data-href="third"]').on('click', function(){
+	$('.page ul li a.third').on('click', function(){
 		swiperV.slideTo(2, 500);
 		swiperH.slideTo(2, 500);
+		$('.page ul li:nth-child(5)').addClass('on')
 	});
-	$('.page a[data-href="prepare"]').on('click', function(){
+	$('.page ul li a.prepare').on('click', function(){
+		swiperV.slideTo(2, 500);
+		swiperH.slideTo(3, 500);
+		$('.page ul li:nth-child(6)').addClass('on')
+	});
+	$('.page ul li a.end').on('click', function(){
 		swiperV.slideTo(3, 500);
-		swiperH.slideTo(2, 500);
-	});
-	$('.page a[data-href="end"]').on('click', function(){
-		swiperV.slideTo(2, 500);
-		swiperH.slideTo(2, 500);
+		$('.page ul li').removeClass('on')
+		$('.page ul li:nth-child(7)').addClass('on')
 	});
 })
